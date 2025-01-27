d99b4b378a0b21598cfa3f27b6a7e7a48b71e6c3
Kanha Dewangan <workkanhadewangan>
2025-01-27 20:53:40 +0530

    hover added

--- Reforks/src/components/Card.jsx
+++ Reforks/src/components/Card.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { IoIosArrowRoundForward } from "react-icons/io";
 
 
-const Card = ({width}) => {
+const Card = ({width,start,para, btn , hover}) => {
   return (
-    <div className={` ${width} `}>'
+    <div className={` ${width}  `}>'
         
-        <div className=' bg-zinc-800    p-5 rounded-xl text-white '>
+        <div className= "bg-zinc-800  hover:bg-violet-500 p-5 rounded-xl  text-white">
             <div className='w-full flex justify-between items-center '> 
                 <h3>
                     Up
@@ -18,10 +18,11 @@ const Card = ({width}) => {
                 
             </div>
             <div className='text-6xl font-bold tracking-tight  '>
-                Start  Project
+               {start==true ? <h1>Start Project</h1> : null}
                 </div>
-                <button className='w-32 rounded-full  mt-5 py-2 px-3  border-2 border-zinc-50'> Contact Us</button>
-                    <p className='text-sm  text-zinc-500 font-medium mt-5'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet laboriosam, nisi, upiditate minus inventore molestias unde, repellendus ipsa alias? Similique recusandae autem ipsum, ad debitis commodi animi?</p>
+                    {btn==false ? <button className='w-32 rounded-full  mt-5 py-2 px-3  border-2 border-zinc-50'> Contact Us</button> : null}
+                {para==true ? <p className='text-sm  text-zinc-500 font-medium mt-5'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet laboriosam, nisi, upiditate minus inventore molestias unde, repellendus ipsa alias? Similique recusandae autem ipsum, ad debitis commodi animi?</p> : null}
+                    
         </div>
         </div>
   )
--- Reforks/src/components/Cards.jsx
+++ Reforks/src/components/Cards.jsx
@@ -6,8 +6,8 @@ const Cards = () => {
     <div>
       <div className="w-full ">
         <div className="max-w-screen-xl mx-auto py-20  flex  gap-4  ">
-          <Card width ={"basis-1/3"} />
-          <Card  width ={"basis-2/3"}/>
+          <Card width ={"basis-1/3"} start ={false} para = {true} btn={true} />
+          <Card  width ={"basis-2/3"} start ={ true} para = {false} btn ={false} hover ={"bg-purple-500"}/>
         </div>
       </div>
     </div>
